192dd5dceb17952e7879821ceeb6a488ef5b05fa
glowbulb <adamg@corp.baenetworks.com>
2017-01-30 14:31:00 -0500

    1

--- main.c
+++ main.c
@@ -6,9 +6,36 @@ typedef struct int_array_t {
     int n[2];
 } int_array_t;
 
+typedef struct char_array_t {
+    char s[999];
+    int l[1];
+} char_array_t;
+
+typedef void (*void_char_array_f_t)(char_array_t a);
+typedef char_array_t (*char_array_f_t)();
+typedef char_array_t (*char_array_char_array_char_array_f_t)(char_array_t a, char_array_t b);
+
 typedef void (*void_int_array_f_t)(int_array_t a);
 typedef int_array_t (*int_array_int_array_f_t)(int_array_t a);
 typedef int_array_t (*int_array_f_t)();
+
+char_array_t new_char_array_f() {
+    const char_array_t n = { .s = "\0", .l = 0 };
+    return n;
+}
+
+char_array_t append_char_array_f(char_array_t a, char_array_t b) {
+
+    char x[10] = "totally";
+    char y[10] = "rekt";
+    char z[20]
+
+    strcpy(x, z);
+    strcat(y, z);
+
+    const char_array_t n = { .s = z, .l = 11 };
+    return n;
+}
 
 int_array_t new_int_array_f() {
     const int_array_t n = { .n = { 1,2 } };
@@ -29,6 +56,10 @@ void print_int_array_f(int_array_t a){
     printf("\n", printf("%d %d\n", a.n[0], a.n[1]));
 }
 
+void print_char_array_f(char_array_t a){
+    printf("%s", a.s);
+}
+
 int_array_t arg_int_array_f(int argc, char** argv) {
     if(argc <= 4 && (strcmp(argv[1], "-print") == 0)){
         const int_array_t n = { .n = { atoi(argv[2]), atoi(argv[3]) } };
@@ -43,10 +74,16 @@ void_int_array_f_t print_int_array = print_int_array_f;
 int_array_f_t new_int_array = new_int_array_f;
 int_array_int_array_f_t addone_int_array = addone_int_array_f;
 
+void_char_array_f_t print_char_array = print_char_array_f;
+char_array_f_t new_char_array = new_char_array_f;
+char_array_char_array_char_array_f_t append_char_array = append_char_array_f;
+
 int main(int argc, char** argv) {
 
-    if(argc < 2) { return 0; }
+//    if(argc < 2) { return 0; }
 
-    print_int_array(addone_int_array(swap_int_array_f(arg_int_array_f(argc, argv))));
+//    print_int_array(addone_int_array(swap_int_array_f(arg_int_array_f(argc, argv))));
 
+    const char_array_t rekt = { .s = "rekt", .l = 4 };
+    print_char_array(append_char_array(rekt, rekt));
 }
